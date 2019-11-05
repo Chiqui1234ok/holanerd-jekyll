233fbf7cf6ae688a4a49fedab1b45ce93e919c59
Chiqui1234 <santiagogimenez@outlook.com.ar>
2019-11-05 16:38:49 -0300

    updated

---
+++ assets/js/components/sidebar.js
@@ -0,0 +1,14 @@
+var sidebar = new Vue({
+    el: '#sidebar',
+    delimiters: ['_', '_'],
+    data: 
+    {
+        li1: 
+        {
+            name: 'Inicio',
+            url: '/'
+        },
+        li2: 'Blog',
+        li3: 'Contacto'
+    }
+})
